e0628bcdd9ec6662fdb77a7360cfa25179387cb3
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-18 05:04:04 +0300

    Update Utils class

--- src/main/java/me/itstautvydas/uuidswapper/Utils.java
+++ src/main/java/me/itstautvydas/uuidswapper/Utils.java
@@ -1,7 +1,10 @@
 package me.itstautvydas.uuidswapper;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.*;
+import com.moandjiezana.toml.Toml;
 import com.velocitypowered.api.util.GameProfile;
+import com.velocitypowered.api.util.UuidUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
@@ -11,6 +14,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Utils {
+    public static final Map<String, Object> EMPTY_MAP = ImmutableMap.of();
+
+    private Utils() {}
+
+    public static GameProfile createProfile(String username, UUID uuid, GameProfile profile) {
+        if (username == null)
+            username = profile.getName();
+        return new GameProfile(uuid == null ? profile.getId() : uuid, username, profile.getProperties());
+    }
+
     public static GameProfile createProfile(String username, String uuid, GameProfile profile) {
         if (username == null)
             username = profile.getName();
@@ -20,9 +33,7 @@ public class Utils {
     public static boolean containsPlayer(List<String> list, String username, UUID id) {
         if (list.contains("\"u:" + username + "\""))
             return true;
-        if (list.contains("\"" + id.toString() + "\""))
-            return true;
-        return false;
+        return list.contains("\"" + id.toString() + "\"");
     }
 
     public static String getSwappedValue(Map<String, Object> map, GameProfile profile) {
@@ -52,14 +63,14 @@ public class Utils {
             return element.isJsonPrimitive() ? element.getAsString() : element.toString();
         String[] parts = path.split("\\.");
         for (String part : parts) {
-            if (part.matches(".+\\[\\d+\\]")) { // Array indexes - key[0]
+            if (part.matches(".+\\[\\d+]")) { // Array indexes - key[0]
                 String key = part.substring(0, part.indexOf('['));
                 int index = Integer.parseInt(part.substring(part.indexOf('[') + 1, part.indexOf(']')));
                 element = element.getAsJsonObject().get(key);
                 if (element == null || !element.isJsonArray())
                     return null;
                 element = element.getAsJsonArray().get(index);
-            } else if (part.matches("\\[\\d+\\]")) { // Direct array indexes - [0]
+            } else if (part.matches("\\[\\d+]")) { // Direct array indexes - [0]
                 int index = Integer.parseInt(part.substring(1, part.indexOf(']')));
                 if (!element.isJsonArray())
                     return null;
@@ -97,16 +108,97 @@ public class Utils {
         if (prefix == null) prefix = "";
         if (suffix == null) suffix = "";
         for (var entry : placeholders.entrySet()) {
-            string = string.replace(prefix + entry.getKey() + suffix, entry.getValue().toString());
+            var value = entry.getValue();
+            if (value == null)
+                value = "null";
+            string = string.replace(prefix + entry.getKey() + suffix, value.toString());
         }
         return string;
     }
 
     public static String replacePlaceholders(String string, Map<String, Object> placeholders) {
-        return replacePlaceholders(string, placeholders, null, null);
+        return replacePlaceholders(string, placeholders, "{", "}");
+    }
+
+    public static void addExceptionPlaceholders(Throwable ex, Map<String, Object> placeholders) {
+        placeholders.put("error.class", ex.getClass().getName());
+        placeholders.put("error.message", ex.getMessage());
+        placeholders.put("error.class-name", ex.getClass().getSimpleName());
     }
 
     public static Component toComponent(String string) {
         return MiniMessage.miniMessage().deserialize(string);
+    }
+
+    public static UUID requireUuid(String username, UUID uuid) {
+        if (uuid == null)
+            return UuidUtils.generateOfflinePlayerUuid(username);
+        return uuid;
+    }
+
+    public static String toMessage(Toml config, String path) {
+        if (config.containsPrimitive(path))
+            return config.getString(path);
+        else if (config.containsTableArray(path)) {
+            List<String> list = config.getList(path);
+            if (list == null)
+                return null;
+            return String.join("\n", list);
+        }
+        return null;
+    }
+
+    // Unused
+//    @SuppressWarnings("unchecked")
+//    public static Toml getTableWithDefaults(String pathToTable, Toml toml, Toml defaultToml) {
+//        Objects.requireNonNull(pathToTable);
+//        Objects.requireNonNull(toml);
+//        Objects.requireNonNull(defaultToml);
+//
+//        try {
+//            var method = toml.getClass().getDeclaredMethod("get", String.class);
+//            method.setAccessible(true);
+//            var map = (Map<String, Object>) method.invoke(toml, pathToTable);
+//
+//            var constructor = toml.getClass().getDeclaredConstructor(Toml.class, Map.class);
+//            constructor.setAccessible(true);
+//
+//            return map != null ? constructor.newInstance(defaultToml, map) : null;
+//        } catch (Exception ex) {
+//            return toml.getTable(pathToTable);
+//        }
+//    }
+
+    @SuppressWarnings("unchecked")
+    public static List<Toml> getTablesWithDefaults(String pathToTable, Toml toml, Toml defaultToml) {
+        Objects.requireNonNull(pathToTable);
+        Objects.requireNonNull(toml);
+        Objects.requireNonNull(defaultToml);
+
+        try {
+            var method = toml.getClass().getDeclaredMethod("get", String.class);
+            method.setAccessible(true);
+            var tableArray = (List<Map<String, Object>>) method.invoke(toml, pathToTable);
+
+            var constructor = toml.getClass().getDeclaredConstructor(Toml.class, Map.class);
+            constructor.setAccessible(true);
+
+            if (tableArray == null)
+                return null;
+
+            var tables = new ArrayList<Toml>();
+            for (Map<String, Object> table : tableArray)
+                tables.add(constructor.newInstance(defaultToml, table));
+
+            return tables;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return toml.getTables(pathToTable);
+        }
+    }
+
+    public static Map<String, Object> getTableOrEmpty(Toml toml, String path) {
+        var table = toml.getTable(path);
+        return table == null ? Utils.EMPTY_MAP : table.toMap();
     }
 }
