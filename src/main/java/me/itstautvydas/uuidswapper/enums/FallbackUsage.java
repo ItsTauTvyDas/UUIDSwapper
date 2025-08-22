d0b0c589f89b6caf76cf01bc0dde90d651ef5cbb
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-22 06:25:01 +0300

    Add enum for stating fallback usage

---
+++ src/main/java/me/itstautvydas/uuidswapper/enums/FallbackUsage.java
@@ -0,0 +1,9 @@
+package me.itstautvydas.uuidswapper.enums;
+
+public enum FallbackUsage {
+    ON_CONNECTION_ERROR,
+    ON_INVALID_UUID,
+    ON_BAD_UUID_PATH,
+    ON_UNKNOWN_ERROR,
+    ON_BAD_STATUS
+}
