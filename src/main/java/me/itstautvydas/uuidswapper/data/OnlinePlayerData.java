b2ff78ab31f62b3677309f96757b73ba3f579f24
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-22 06:31:13 +0300

    Remove FetchedPlayerData class usage

--- src/main/java/me/itstautvydas/uuidswapper/data/OnlinePlayerData.java
+++ src/main/java/me/itstautvydas/uuidswapper/data/OnlinePlayerData.java
@@ -5,23 +5,22 @@ import lombok.Getter;
 import java.util.List;
 import java.util.UUID;
 
+@SuppressWarnings("ClassCanBeRecord")
 @Getter
-public class OnlinePlayerData extends FetchedPlayerData {
+public class OnlinePlayerData {
+    private final UUID originalUuid;
+    private final UUID onlineUuid;
+    private final List<ProfileProperty> properties;
     private final String address;
     private final Long createdAt;
     private final Long updatedAt;
 
     public OnlinePlayerData(UUID originalUuid, UUID onlineUuid, List<ProfileProperty> properties, String address, Long createdAt, Long updatedAt) {
-        super(originalUuid, onlineUuid, properties);
+        this.originalUuid = originalUuid;
+        this.onlineUuid = onlineUuid;
+        this.properties = properties;
         this.address = address;
         this.createdAt = createdAt == null ? System.currentTimeMillis() : createdAt;
         this.updatedAt = updatedAt == null ? System.currentTimeMillis() : updatedAt;
-    }
-
-    public OnlinePlayerData(UUID originalUuid, UUID onlineUuid, List<ProfileProperty> properties, String address) {
-        super(originalUuid, onlineUuid, properties);
-        this.address = address;
-        this.createdAt = System.currentTimeMillis();
-        this.updatedAt = System.currentTimeMillis();
     }
 }
