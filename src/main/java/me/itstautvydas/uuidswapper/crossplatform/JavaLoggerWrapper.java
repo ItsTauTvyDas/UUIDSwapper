db6db3bc0d055689174a6a0a37525bf07547e9bd
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-24 06:28:10 +0300

    Add wrapper for java logger

---
+++ src/main/java/me/itstautvydas/uuidswapper/crossplatform/JavaLoggerWrapper.java
@@ -0,0 +1,25 @@
+package me.itstautvydas.uuidswapper.crossplatform;
+
+import me.itstautvydas.uuidswapper.Utils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class JavaLoggerWrapper<P, T, S, M>  extends PluginWrapper<P, T, Logger, S, M>  {
+    @Override
+    public void logInfo(String prefix, String message, Object... args) {
+        logger.log(Level.INFO, Utils.toLoggerMessage(prefix, message), args);
+    }
+
+    @Override
+    public void logWarning(String prefix, String message, Throwable exception, Object... args) {
+        logger.log(Level.WARNING, Utils.toLoggerMessage(prefix, message), args);
+        Utils.printException(exception, logger::warning);
+    }
+
+    @Override
+    public void logError(String prefix, String message, Throwable exception, Object... args) {
+        logger.log(Level.SEVERE, Utils.toLoggerMessage(prefix, message), args);
+        Utils.printException(exception, logger::severe);
+    }
+}
