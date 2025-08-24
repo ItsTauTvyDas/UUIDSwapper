c0af789f0616baa6c53cee40fb97e6f33693d7d9
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-24 06:31:26 +0300

    Update utils class

--- src/main/java/me/itstautvydas/uuidswapper/Utils.java
+++ src/main/java/me/itstautvydas/uuidswapper/Utils.java
@@ -8,19 +8,30 @@ import me.itstautvydas.uuidswapper.crossplatform.PluginWrapper;
 import me.itstautvydas.uuidswapper.enums.PlatformType;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @UtilityClass
 public class Utils {
     private static final Pattern LOG_MESSAGE_BRACKETS = Pattern.compile("\\{}");
+    private static final String PLACEHOLDER_PREFIX = "{";
+    private static final String PLACEHOLDER_SUFFIX = "}";
+
+    public static final String COMMAND_PERMISSION = "uuidswapper.command";
+    public static final String RELOAD_COMMAND_PERMISSION = "uuidswapper.command.reload";
+    public static final String DEBUG_COMMAND_PERMISSION = "uuidswapper.command.debug";
+    public static final String PRETEND_COMMAND_PERMISSION = "uuidswapper.command.pretend";
 
     public static String getSwappedValue(Map<String, String> map, String username, UUID uniqueId) {
         var value = map.get("u:" + username);
@@ -88,20 +99,16 @@ public class Utils {
         }
     }
 
-    public static String replacePlaceholders(String string, Map<String, Object> placeholders, String prefix, String suffix) {
-        if (prefix == null) prefix = "";
-        if (suffix == null) suffix = "";
+    public static String replacePlaceholders(String string, Map<String, Object> placeholders) {
+        if (placeholders == null)
+            return string;
         for (var entry : placeholders.entrySet()) {
             var value = entry.getValue();
             if (value == null)
                 value = "null";
-            string = string.replace(prefix + entry.getKey() + suffix, value.toString());
+            string = string.replace(PLACEHOLDER_PREFIX + entry.getKey() + PLACEHOLDER_SUFFIX, value.toString());
         }
         return string;
-    }
-
-    public static String replacePlaceholders(String string, Map<String, Object> placeholders) {
-        return replacePlaceholders(string, placeholders, "{", "}");
     }
 
     public static void addExceptionPlaceholders(Throwable ex, Map<String, Object> placeholders) {
@@ -116,16 +123,72 @@ public class Utils {
 
     public static UUID requireUuid(String username, UUID uuid) {
         if (uuid == null)
-            return PluginWrapper.getCurrent().generateOfflinePlayerUuid(username);
+            return Utils.generateOfflineUniqueId(username);
         return uuid;
     }
 
-    public static String fixLogMessageFormat(String message, PlatformType type) {
-        if (type == PlatformType.BUNGEE) {
-            var counter = new AtomicInteger(0);
-            var matcher = LOG_MESSAGE_BRACKETS.matcher(message);
-            return matcher.replaceAll(match -> "{" + counter.getAndIncrement() + "}");
+    public void printException(Throwable exception, Consumer<String> onMessageLine) {
+        if (exception == null) return;
+        exception.printStackTrace(new PrintWriter(new Writer() {
+            @Override
+            public void write(char @NotNull []cbuf, int off, int len) {
+                var str = new String(cbuf, off, len).trim();
+                if (str.isBlank())
+                    return;
+                onMessageLine.accept(str);
+            }
+
+            @Override
+            public void flush() {}
+
+            @Override
+            public void close() {}
+        }));
+    }
+
+    public UUID generateOfflineUniqueId(String username) {
+        return UUID.nameUUIDFromBytes(("OfflinePlayer:" + username).getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String toLoggerMessage(String prefix, String message) {
+        if (PluginWrapper.getCurrent() != null) {
+            var type = PluginWrapper.getCurrent().getPlatformType();
+            if (type == PlatformType.BUNGEE || type == PlatformType.PAPER) {
+                var counter = new AtomicInteger(0);
+                var matcher = LOG_MESSAGE_BRACKETS.matcher(message);
+                message = matcher.replaceAll(match -> "{" + counter.getAndIncrement() + "}");
+            }
         }
-        return message;
+        if (prefix == null)
+            return message;
+        return "[" + prefix + "]: " + message;
+    }
+
+    // https://stackoverflow.com/questions/34092373/merge-extend-json-objects-using-gson-in-java
+    public static void merge(JsonObject defaultConfiguration, JsonObject currentConfiguration) {
+        for (Map.Entry<String, JsonElement> rightEntry : defaultConfiguration.entrySet()) {
+            String rightKey = rightEntry.getKey();
+            JsonElement rightVal = rightEntry.getValue();
+            if (currentConfiguration.has(rightKey)) {
+                // conflict
+                JsonElement leftVal = currentConfiguration.get(rightKey);
+                if (leftVal.isJsonArray() && rightVal.isJsonArray()) {
+                    JsonArray leftArr = leftVal.getAsJsonArray();
+                    JsonArray rightArr = rightVal.getAsJsonArray();
+                    // concat the arrays -- there cannot be a conflict in an array, it's just a collection of stuff
+                    for (int i = 0; i < rightArr.size(); i++)
+                        leftArr.add(rightArr.get(i));
+                } else if (leftVal.isJsonObject() && rightVal.isJsonObject()) {
+                    // recursive merging
+                    merge(rightVal.getAsJsonObject(), leftVal.getAsJsonObject());
+                } else {// not both arrays or objects, normal merge with conflict resolution
+                    if (leftVal.isJsonNull() && !rightVal.isJsonNull()) {
+                        currentConfiguration.add(rightKey, rightVal);
+                    }
+                }
+            } else { // no conflict, add to the object
+                currentConfiguration.add(rightKey, rightVal);
+            }
+        }
     }
 }
