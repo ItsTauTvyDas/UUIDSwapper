674325e25e108f2de23f9679428bf162ef7e43bd
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-24 06:27:47 +0300

    Add BungeeCord plugin wrapper

---
+++ src/main/java/me/itstautvydas/uuidswapper/crossplatform/wrapper/BungeeCordPluginWrapper.java
@@ -0,0 +1,53 @@
+package me.itstautvydas.uuidswapper.crossplatform.wrapper;
+
+import me.itstautvydas.uuidswapper.Utils;
+import me.itstautvydas.uuidswapper.config.Configuration;
+import me.itstautvydas.uuidswapper.crossplatform.JavaLoggerWrapper;
+import me.itstautvydas.uuidswapper.crossplatform.PluginTaskWrapper;
+import me.itstautvydas.uuidswapper.loader.UUIDSwapperBungeeCord;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.scheduler.ScheduledTask;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+public class BungeeCordPluginWrapper extends JavaLoggerWrapper<UUIDSwapperBungeeCord, ScheduledTask, ProxyServer, CommandSender> {
+    @Override
+    public void sendMessage(CommandSender sender, Function<Configuration.CommandMessagesConfiguration, String> message, Map<String, Object> placeholders) {
+        sender.sendMessage(TextComponent.fromLegacy(Utils.replacePlaceholders(message.apply(getConfiguration().getCommandMessages()), placeholders)));
+    }
+
+    @Override
+    public void registerCommand() {
+        ProxyServer.getInstance().getPluginManager().registerCommand(handle, new Command("uuidswapper-bungee") {
+            @Override
+            public void execute(CommandSender sender, String[] args) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean isServerOnlineMode() {
+        return server.getConfig().isOnlineMode();
+    }
+
+    @Override
+    public PluginTaskWrapper<ScheduledTask> scheduleTask(Runnable run, Long repeatInSeconds, long delayInSeconds) {
+        ScheduledTask task;
+        if (repeatInSeconds == null)
+            task = server.getScheduler().schedule(handle, run, delayInSeconds, TimeUnit.SECONDS);
+        else
+            task = server.getScheduler().schedule(handle, run, delayInSeconds, repeatInSeconds, TimeUnit.SECONDS);
+        return new PluginTaskWrapper<>(task) {
+            @Override
+            public void cancel() {
+                handle.cancel();
+            }
+        };
+    }
+}
