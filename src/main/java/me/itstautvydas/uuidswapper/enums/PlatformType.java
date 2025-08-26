e94ae93e2967d7923cffac47376e92c5fef0e731
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-26 07:27:47 +0300

    Add FOLIA enum

--- src/main/java/me/itstautvydas/uuidswapper/enums/PlatformType.java
+++ src/main/java/me/itstautvydas/uuidswapper/enums/PlatformType.java
@@ -2,12 +2,20 @@ package me.itstautvydas.uuidswapper.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import me.itstautvydas.uuidswapper.Utils;
 
 @RequiredArgsConstructor
 @Getter
 public enum PlatformType {
     VELOCITY("Velocity"),
     BUNGEE("BungeeCord"),
-    PAPER("Paper");
+    PAPER("Paper"),
+    FOLIA("Folia");
     final String name;
+
+    public PlatformType verifyFolia() {
+        if (this == PAPER && Utils.isRunningFolia())
+            return FOLIA;
+        return this;
+    }
 }
