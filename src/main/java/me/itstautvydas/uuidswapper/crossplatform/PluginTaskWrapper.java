bdcde424af9f0c0371797a055f9a01f6cd6f91e0
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-26 07:28:40 +0300

    Remove generic parameter

--- src/main/java/me/itstautvydas/uuidswapper/crossplatform/PluginTaskWrapper.java
+++ src/main/java/me/itstautvydas/uuidswapper/crossplatform/PluginTaskWrapper.java
@@ -3,8 +3,8 @@ package me.itstautvydas.uuidswapper.crossplatform;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public abstract class PluginTaskWrapper<T> {
-    protected final T handle;
+public abstract class PluginTaskWrapper {
+    protected final Object handle;
 
     public abstract void cancel();
 }
