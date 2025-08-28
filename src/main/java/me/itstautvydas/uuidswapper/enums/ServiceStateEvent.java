fc21d3a727b57940be470fb4a25666158242f562
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-28 06:40:04 +0300

    Rename

---
+++ src/main/java/me/itstautvydas/uuidswapper/enums/ServiceStateEvent.java
@@ -0,0 +1,11 @@
+package me.itstautvydas.uuidswapper.enums;
+
+public enum ServiceStateEvent {
+    PRE_REQUEST,
+    POST_REQUEST,
+    FETCHED_UUID,
+    PRE_PROPERTIES_SERVICE_FETCH,
+    FETCHED_PROPERTIES,
+    PRE_FALLBACK_USE,
+    SERVICE_SUCCESS
+}
