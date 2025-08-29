0777665ad4df745c8a505771795282775035b145
Tautvydas <tautvydas.gaming@gmail.com>
2025-08-29 19:37:19 +0300

    Update InvalidFieldsCollectorAdapterFactory class

--- src/main/java/me/itstautvydas/uuidswapper/json/InvalidFieldsCollectorAdapterFactory.java
+++ src/main/java/me/itstautvydas/uuidswapper/json/InvalidFieldsCollectorAdapterFactory.java
@@ -5,57 +5,67 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.internal.bind.ReflectiveTypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
-import me.itstautvydas.uuidswapper.crossplatform.PluginWrapper;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-// https://github.com/google/gson/issues/188 (keeping original comments)
+// https://github.com/google/gson/issues/188 (modified)
 // IT HAS BEEN 10 YEARS COME ON GOOGLE DO SOMETHING
+
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class InvalidFieldsCollectorAdapterFactory implements TypeAdapterFactory {
 
-    public static final Map<Gson, List<Object>> INVALID_FIELDS = new HashMap<>();
+    private static final ThreadLocal<JsonReader> CURRENT_READER = new ThreadLocal<>();
 
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
-        // If the type adapter is a reflective type adapter, we want to modify the implementation using reflection. The
-        // trick is to replace the Map object used to lookup the property name. Instead of returning null if the
-        // property is not found, we throw a Json exception to terminate the deserialization.
         TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
 
-        // Check if the type adapter is a reflective, cause this solution only work for reflection.
         if (delegate instanceof ReflectiveTypeAdapterFactory.Adapter delegateAdapter) {
             try {
-                // Get reference to the existing boundFields.
                 Field f = findField(delegateAdapter.getClass(), "boundFields");
                 f.setAccessible(true);
-                Map boundFields = (Map) f.get(delegate);
-
-                // Then replace it with our implementation throwing exception if the value is null.
-                boundFields = new LinkedHashMap(boundFields) {
+                f.set(delegate, new LinkedHashMap((Map) f.get(delegate)) {
                     @Override
                     public Object get(Object key) {
                         Object value = super.get(key);
+                        System.out.println(key + " - " + value + " at " + CURRENT_READER.get().getPath());
                         if (value == null)
-                            INVALID_FIELDS.get(gson).add(key);
+                            ConfigurationErrorCollector.collect(
+                                    gson,
+                                    ConfigurationErrorCollector.INVALID_PROPERTY,
+                                    key,
+                                    CURRENT_READER.get(),
+                                    false
+                            );
                         return value;
-
                     }
-                };
-                // Finally, push our custom map back using reflection.
-                f.set(delegate, boundFields);
+                });
             } catch (Exception e) {
-                // Should never happen if the implementation doesn't change.
-//                throw new IllegalStateException(e);
-                PluginWrapper.getCurrent().logInfo("(Ignore) Failed to collect invalid configuration fields - " + e.getMessage());
+                // Do nothing
             }
         }
-        return delegate;
+        return new TypeAdapter<>() {
+            @Override
+            public void write(JsonWriter out, T value) throws IOException {
+                delegate.write(out, value);
+            }
+
+            @Override
+            public T read(JsonReader in) throws IOException {
+                CURRENT_READER.set(in);
+                try {
+                    return delegate.read(in);
+                } finally {
+                    CURRENT_READER.remove();
+                }
+            }
+        };
     }
 
     private static Field findField(@NotNull Class<?> startingClass, String fieldName) throws NoSuchFieldException {
@@ -63,7 +73,6 @@ public class InvalidFieldsCollectorAdapterFactory implements TypeAdapterFactory 
             try {
                 return c.getDeclaredField(fieldName);
             } catch (NoSuchFieldException e) {
-                // OK: continue with superclasses
             }
         }
         throw new NoSuchFieldException(fieldName + " starting from " + startingClass.getName());
