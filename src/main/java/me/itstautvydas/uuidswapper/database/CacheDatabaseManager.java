9ec0ab652f77acdfd18ed0d02e08570af051a3e6
Tautvydas <tautvydas.gaming@gmail.com>
2025-09-06 13:03:03 +0300

    Update database manager class

--- src/main/java/me/itstautvydas/uuidswapper/database/CacheDatabaseManager.java
+++ src/main/java/me/itstautvydas/uuidswapper/database/CacheDatabaseManager.java
@@ -2,85 +2,31 @@ package me.itstautvydas.uuidswapper.database;
 
 import lombok.Getter;
 import me.itstautvydas.uuidswapper.config.Configuration;
-import me.itstautvydas.uuidswapper.crossplatform.PluginTaskWrapper;
-import me.itstautvydas.uuidswapper.crossplatform.PluginWrapper;
+import me.itstautvydas.uuidswapper.data.DatabaseObject;
+import me.itstautvydas.uuidswapper.data.PlayerData;
+import me.itstautvydas.uuidswapper.multiplatform.MultiPlatform;
 import me.itstautvydas.uuidswapper.data.OnlinePlayerData;
 
-import java.util.Objects;
+import java.util.*;
 
 public class CacheDatabaseManager {
     @Getter
     private DriverImplementation driver;
-    private long timeCounter;
-    private PluginTaskWrapper timer;
 
-    public void shutdown() {
-        clean();
-    }
-
-    private void clean() {
-        if (timer != null)
-            timer.cancel();
+    public void clear() {
         if (driver != null) {
-            if (!driver.supportsCaching)
-                return;
-            clearConnection();
+            try {
+                driver.clean();
+            } catch (Exception ex) {
+                driver.error("Exception caught while cleaning up", ex);
+            }
         }
-    }
-
-    public void clearConnection() {
-        if (driver == null)
-            return;
-        if (!driver.supportsCaching)
-            return;
-        try {
-            driver.debug("Trying to close connection");
-            if (!driver.clearConnection())
-                driver.debug("Connection was not cached");
-            driver.debug("CONNECTION CLOSED");
-        } catch (Exception ex) {
-            PluginWrapper.getCurrent().logError("DatabaseManager", "Failed to close %s driver's connection!", ex, driver.getName());
-        }
-    }
-
-    public void resetTimer() {
-        resetCounter();
-        if (timer != null) {
-            timer.cancel();
-            timer = null;
-        }
-        if (driver != null && !driver.supportsCaching)
-            return;
-        if (driver instanceof CacheableConnectionDriverImplementation cacheable) {
-            var keepOpen = cacheable.getKeepOpenTime();
-            if (keepOpen <= 0)
-                return;
-            var repeat = cacheable.getTimerRepeatTime();
-            timer = PluginWrapper.getCurrent().scheduleTask(() -> {
-                if (timeCounter == -1)
-                    return;
-                timeCounter += repeat;
-                if (timeCounter > keepOpen) {
-                    clearConnection();
-                    lockCounter();
-                }
-            }, repeat, 0);
-        }
-    }
-
-    public void resetCounter() {
-        timeCounter = 0;
-    }
-
-    public void lockCounter() {
-        timeCounter = -1;
     }
 
     public boolean loadDriverFromConfiguration() {
-        clean();
         var name = getConfiguration().getDriverName();
         if (setDriverImplementation(name) == null) {
-            PluginWrapper.getCurrent().logError("DatabaseManager", "Failed to load %s driver!", null, name);
+            MultiPlatform.get().logError("DatabaseManager", "Failed to load %s driver!", null, name);
             return false;
         }
         return true;
@@ -96,53 +42,133 @@ public class CacheDatabaseManager {
     public boolean setDriverImplementation(DriverImplementation driver) {
         try {
             Objects.requireNonNull(driver);
-            if (!driver.init())
-                return false;
-            driver.debug("Connection initialization.");
-            if (driver instanceof CacheableConnectionDriverImplementation cacheable) {
-                driver.debug("Connection timeout => %s", cacheable.getTimeout());
-                driver.debug("Connection always kept => %s", cacheable.shouldConnectionBeAlwaysKept());
-                driver.debug("Connection cached => %s", cacheable.shouldConnectionBeCached());
-            }
+            if (!driver.init()) return false;
 
-            if (driver.connectionBased) {
+            driver.info("%s (%s) initialization.", driver.getName(), driver.getClass().getName());
+
+            var json = driver.toJson();
+            for (var key : json.keySet())
+                driver.debug("%s => %s", key, json.get(key));
+
+            if (driver instanceof TableBasedDriver tableBasedDriver) {
                 try {
                     driver.debug("Trying to create %s table", DriverImplementation.ONLINE_UUID_CACHE_TABLE);
-                    driver.createOnlineUuidCacheTable();
+                    tableBasedDriver.createOnlineUuidCacheTable();
                 } catch (Exception ex) {
-                    PluginWrapper.getCurrent().logError("Failed to create %s table", ex, DriverImplementation.ONLINE_UUID_CACHE_TABLE);
+                    driver.error("Failed to create %s table", ex, DriverImplementation.ONLINE_UUID_CACHE_TABLE);
                 }
 
                 try {
-                    driver.createRandomizedPlayerDataTable();
+                    tableBasedDriver.createRandomizedPlayerDataTable();
                 } catch (Exception ex) {
-                    PluginWrapper.getCurrent().logError("Failed to create %s table", ex, DriverImplementation.RANDOM_PLAYER_CACHE_TABLE);
+                    driver.error("Failed to create %s table", ex, DriverImplementation.RANDOM_PLAYER_CACHE_TABLE);
                 }
             }
         } catch (Exception ex) {
-            PluginWrapper.getCurrent().logError(driver.getPrefix(), "Failed to initialize %s driver!", ex, driver.getName());
+            driver.error("Failed to initialize %s driver!", ex, driver.getName());
             return false;
         }
         this.driver = driver;
         return true;
     }
 
-    public void storeOnlinePlayerCache(OnlinePlayerData player) {
+    public boolean storeOnlinePlayerCache(OnlinePlayerData player) {
         try {
             driver.debug("Trying to store online player (original UUID => %s)", player.getOriginalUniqueId());
             driver.storeOnlinePlayerCache(player);
+            return true;
         } catch (Exception ex) {
-            PluginWrapper.getCurrent().logInfo(driver.getPrefix(), "Failed to store online player database for %s", ex, player.getOriginalUniqueId());
+            driver.error("Failed to store online player database for %s", ex, player.getOriginalUniqueId());
+            return false;
         }
     }
 
+    public boolean storeRandomPlayerCache(PlayerData player) {
+        try {
+            driver.debug("Trying to store random player (original UUID => %s)", player.getOriginalUniqueId());
+            driver.storeRandomPlayerCache(player);
+            return true;
+        } catch (Exception ex) {
+            driver.error("Failed to store random player database for %s", ex, player.getOriginalUniqueId());
+            return false;
+        }
+    }
+
+    public DatabaseObject<List<OnlinePlayerData>> getOnlinePlayersCache() {
+        List<OnlinePlayerData> data = null;
+        Exception exception = null;
+        try {
+            data = driver.getOnlinePlayersCache();
+        } catch (Exception ex) {
+            exception = ex;
+        }
+        return new DatabaseObject<>(data, exception, driver, null) {
+            @Override
+            public DatabaseObject<List<OnlinePlayerData>> printErrorIfAny() {
+                driver.error("Failed to get all stored online players", exception);
+                return this;
+            }
+        };
+    }
+
+    public DatabaseObject<List<PlayerData>> getRandomPlayersCache() {
+        List<PlayerData> data = null;
+        Exception exception = null;
+        try {
+            data = driver.getRandomPlayersCache();
+        } catch (Exception ex) {
+            exception = ex;
+        }
+        return new DatabaseObject<>(data, exception, driver, null) {
+            @Override
+            public DatabaseObject<List<PlayerData>> printErrorIfAny() {
+                driver.error("Failed to get all stored random players", exception);
+                return this;
+            }
+        };
+    }
+
+    public DatabaseObject<PlayerData> getRandomPlayerCache(UUID uuid) {
+        PlayerData data = null;
+        Exception exception = null;
+        try {
+            data = driver.getRandomPlayerCache(uuid);
+        } catch (Exception ex) {
+            exception = ex;
+        }
+        return new DatabaseObject<>(data, exception, driver, uuid) {
+            @Override
+            public DatabaseObject<PlayerData> printErrorIfAny() {
+                driver.error("Failed to get random player data (%s) from the database", exception, key);
+                return this;
+            }
+        };
+    }
+
+    public DatabaseObject<OnlinePlayerData> getOnlinePlayerCache(UUID uuid) {
+        OnlinePlayerData data = null;
+        Exception exception = null;
+        try {
+            data = driver.getOnlinePlayerCache(uuid);
+        } catch (Exception ex) {
+            exception = ex;
+        }
+        return new DatabaseObject<>(data, exception, driver, uuid) {
+            @Override
+            public DatabaseObject<OnlinePlayerData> printErrorIfAny() {
+                driver.error("Failed to get online player data (%s) from the database", exception, key);
+                return this;
+            }
+        };
+    }
+
     public Configuration.DatabaseConfiguration getConfiguration() {
-        return PluginWrapper.getCurrent().getConfiguration().getDatabase();
+        return MultiPlatform.get().getConfiguration().getDatabase();
     }
 
     public boolean isDriverRunning() {
         try {
-            return driver != null && !driver.isConnectionClosed();
+            return driver != null && driver.isRunning();
         } catch (Exception ex) {
             return false;
         }
